ae08e3780da06b79d66a114a348f8e4bccc7f25c
Cyborg2017 <wangyang0987654@gmail.com>
2019-02-22 10:57:40 +0800

    g3-common: Define vendor security patch level

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -51,3 +51,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.qualcomm.sns.daemon=e \
     debug.qualcomm.sns.hal=e \
     debug.qualcomm.sns.libsensor1=e
+
+# Vendor security patch level
+ro.lineage.build.vendor_security_patch=2017-06-01
+ro.vendor.build.security_patch=2017-06-01
